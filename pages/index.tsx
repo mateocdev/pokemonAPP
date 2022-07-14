88a5c45ff5914bc32dc5a3562f22583695bdfdbc
Mateo <ivan.castano@globant.com>
2022-07-14 16:59:00 -0500

    [WIP]Adding dark theme

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@nextui-org/react";
 import type { NextPage } from "next";
 
 const HomePage: NextPage = () => {
   return (
     <>
       <h1>hello world</h1>
+      <Button color="gradient">hello world</Button>
     </>
   );
 };
