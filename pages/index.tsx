38877d9ce59cdc49a8d99aaaa4075913b26128d1
Mateo <ivan.castano@globant.com>
2022-07-19 21:43:28 -0500

    Adding axios and ssr rendering page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,29 @@
 import type { NextPage } from "next";
 import { Layout } from "../components/layouts";
+import { GetStaticProps } from "next";
+import pokeApi from '../api/pokeApi';
 
-const HomePage: NextPage = () => {
+const HomePage: NextPage = ({ data }) => {
+  console.log(data);
   return (
-      <Layout>
-        hola mundo
-      </Layout>
+    <Layout title="Pokemon List">
+      <ul>Pokemon</ul>
+      <ul>Pokemon</ul>
+      <ul>Pokemon</ul>
+      <ul>Pokemon</ul>
+    </Layout>
   );
 };
 
+// getstaticprops is a function that is called when the page is loaded
+// and it is used to fetch data from the server
+export const getStaticProps = async () => {
+  const { data: { results } = {} } = await pokeApi.get("/pokemon?limit=151") || {};
+  console.log(results);
+  return {
+    props: {
+      data: results,
+    },
+  }; 
+}
 export default HomePage;
