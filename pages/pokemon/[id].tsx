67bd5096cec20d0d9101d663b9f49c70a7bf03ec
Mateo <ivan.castano@globant.com>
2022-07-22 19:15:37 -0500

    Implementing page of id for pokemons

---
+++ pages/pokemon/[id].tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Layout } from '../../components/layouts'
+
+export const PokemonPage = () => {
+  
+  return (
+    <Layout title="PokemonPage">
+        <h1>Pokemon Page</h1>
+    </Layout>
+  )
+}
